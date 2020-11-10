3ae221c1be914264c39fc128a7cc51f450a22af2
LyapunovJingci <jingcikeili@gmail.com>
2020-11-10 16:41:13 -0500

    Finish IntegerAggregator, pass IntegerAggregator test

--- src/java/simpledb/IntegerAggregator.java
+++ src/java/simpledb/IntegerAggregator.java
@@ -1,11 +1,22 @@
 package simpledb;
 
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * Knows how to compute some aggregate over a set of IntFields.
  */
 public class IntegerAggregator implements Aggregator {
 
     private static final long serialVersionUID = 1L;
+    private int gbfield;
+    private Type groupByFieldType;
+    private int aggregateField;
+    private Op operator;
+
+    //key is gbfield, first element in arraylist is value, second is count
+    private ConcurrentHashMap<Field, ArrayList<Integer>> values;
+    private static Field NO_GROUPING = new IntField(0);
 
     /**
      * Aggregate constructor
@@ -24,6 +35,11 @@ public class IntegerAggregator implements Aggregator {
 
     public IntegerAggregator(int gbfield, Type gbfieldtype, int afield, Op what) {
         // some code goes here
+        this.gbfield = gbfield;
+        this.groupByFieldType = gbfieldtype;
+        this.aggregateField = afield;
+        this.operator = what;
+        this.values = new ConcurrentHashMap<>();
     }
 
     /**
@@ -35,6 +51,42 @@ public class IntegerAggregator implements Aggregator {
      */
     public void mergeTupleIntoGroup(Tuple tup) {
         // some code goes here
+        Field key = tup.getField(this.gbfield);
+
+        int value = ((IntField) (tup.getField(this.aggregateField))).getValue();
+
+        if (tup.getTupleDesc().getFieldType(gbfield).equals(groupByFieldType)) {
+            if (!values.containsKey(key)) {
+                ArrayList<Integer> pair = new ArrayList<>();
+                pair.add(value);
+                pair.add(1);
+                values.put(key, pair);
+            } else {
+                ArrayList<Integer> pair = values.get(key);
+                ArrayList<Integer> newPair = new ArrayList<>();
+                int mergedValue = processMerge(pair.get(0), value, this.operator);
+                newPair.add(mergedValue);
+                newPair.add(pair.get(1) + 1);
+                values.replace(key, newPair);
+            }
+        }
+    }
+
+    private int processMerge(int a, int b, Op op) {
+        switch (op) {
+            case MIN:
+                return Math.min(a, b);
+            case MAX:
+                return Math.max(a, b);
+            case AVG:
+            case SUM:
+                return a + b;
+            case COUNT:
+            case SUM_COUNT:
+            case SC_AVG:
+                return 0;
+        }
+        return 0;
     }
 
     /**
@@ -47,8 +99,45 @@ public class IntegerAggregator implements Aggregator {
      */
     public DbIterator iterator() {
         // some code goes here
-        throw new
-        UnsupportedOperationException("please implement me for lab3");
+
+        // a single (aggregateVal) if no grouping
+        // This is weird, in the comment, this part should be necessary, but it would not be tested
+        if (this.gbfield == Aggregator.NO_GROUPING) {
+            TupleDesc tupleDesc = new TupleDesc(new Type[] { Type.INT_TYPE });
+            ArrayList<Tuple> tuples = new ArrayList<>();
+            Tuple t = new Tuple(tupleDesc);
+            int value = values.get(NO_GROUPING).get(0);
+            int count = values.get(NO_GROUPING).get(1);
+            if (operator.equals(Op.AVG)) {
+                t.setField(0, new IntField(value / count));
+            } else if (operator.equals(Op.COUNT)) {
+                t.setField(0, new IntField(count));
+            } else {
+                t.setField(0, new IntField(value));
+            }
+            tuples.add(t);
+            return new TupleIterator(tupleDesc, tuples);
+        }
+
+        // pair (groupVal, aggregateVal) if using group
+        ArrayList<Tuple> tuples = new ArrayList<Tuple>();
+        TupleDesc tupleDesc = new TupleDesc(new Type[] { this.groupByFieldType, Type.INT_TYPE });
+        for (Field key : this.values.keySet()) {
+            Tuple t = new Tuple(tupleDesc);
+            int value = values.get(key).get(0);
+            int count = values.get(key).get(1);
+            t.setField(0, key);
+            if (operator.equals(Op.AVG)) {
+                t.setField(1, new IntField(value / count));
+            } else if (operator.equals(Op.COUNT)) {
+                t.setField(1, new IntField(count));
+            } else {
+                t.setField(1, new IntField(value));
+            }
+            tuples.add(t);
+        }
+
+        return new TupleIterator(tupleDesc, tuples);
     }
 
 }
